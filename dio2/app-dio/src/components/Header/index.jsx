bdc899207f2cbf7e56a1178f1376a8664d008075
Douglas Rodrigues <dlorodriguesweb3@gmail.com>
2024-11-17 18:20:25 -0300

    React
    Tela home, criando tela login e componentes

--- dio2/app-dio/src/components/Header/index.jsx
+++ dio2/app-dio/src/components/Header/index.jsx
@@ -3,30 +3,42 @@ import logo from '../../assets/logo-dio.png';
 import { Button } from '../Button';
 
 import {
-    Container,
-    Row,
-    Wrapper,
     BuscarInputContainer,
+    Container,
+    Input,
     Menu,
     MenuRight,
+    Row,
+    UserPicture,
+    Wrapper
 } from './styles';
 
-const Header = () => {
+const Header = (autenticado) => {
     return (
         <Wrapper>
             <Container>
                 <Row>
                     <img src={ logo } alt="Logo da Dio" />
-                    <BuscarInputContainer>
-                        <input type="text" placeholder="Buscar..." />
-                    </BuscarInputContainer>
-                    <Menu>Live Code</Menu>
-                    <Menu>Global</Menu>
+                    {autenticado ? (
+                        <>
+                            <BuscarInputContainer>
+                                <Input placeholder="Buscar..." />
+                            </BuscarInputContainer>
+                            <Menu>Live Code</Menu>
+                            <Menu>Global</Menu>
+                        </>
+                    ) : null}
                 </Row>
                 <Row>
-                    <MenuRight href="#">Faq</MenuRight>
-                    <Button title="Entrar" />
-                    <Button title="Cadastrar" />
+                    {autenticado ? (
+                        <UserPicture src="https://avatars.githubusercontent.com/u/103570357?v=4" />
+                    ) : (
+                        <>
+                            <MenuRight href="#">Home</MenuRight>
+                            <Button title="Entrar" />
+                            <Button title="Cadastrar" />
+                        </>
+                    )}
                 </Row>
             </Container>
         </Wrapper>
